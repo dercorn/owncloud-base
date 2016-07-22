b34ef12f0d357000a42e0aa0120aeaf690e5b9f5
Felix Böhm <felixboehm@gmx.de>
2016-07-22 21:07:47 +0200

    fix setup script

--- bin/container-start.sh
+++ bin/container-start.sh
@@ -3,16 +3,19 @@
 if [[ ! -f /mnt/data/config/config.php ]]
 then
   echo "Installing"
-
+  rm -rf /var/www/owncloud/config
   cp /root/config.php /mnt/data/config/config.php
-	ln -s /mnt/data/config /var/www/owncloud/config
+  ln -s /mnt/data/config /var/www/owncloud/config
   chown -R www-data:www-data /var/www/owncloud
 
   owncloud-config.sh
 else
   # offer backups here, optional
 
-  echo "Updating"
+  rm -rf /var/www/owncloud/config
+  su www-data -s /bin/bash -c "ln -s /mnt/data/config /var/www/owncloud/config"
+
+  echo "Update if needed"
   occ upgrade --skip-migration-test --no-interaction
 fi
 
