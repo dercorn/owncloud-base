c369db9f192df78b2c025b74b4f8379ca1597903
Felix Böhm <felixboehm@gmx.de>
2016-07-22 18:38:15 +0200

    start server after install / update

--- bin/container-start.sh
+++ bin/container-start.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Start apache
-service apache2 start
-
 if [[ ! -f /var/www/owncloud/config/config.php ]]
 then
+  echo "Installing"
+
   mkdir -p /var/www/owncloud/config
   cp /root/config.php /var/www/owncloud/config/config.php
   chown -R www-data:www-data /var/www/owncloud
-  echo "Installing"
+
   owncloud-config.sh
 else
+  # offer backups here, optional
+
+  echo "Updating"
   occ upgrade --skip-migration-test --no-interaction
 fi
+
+# Start apache
+service apache2 start
+
+# Start cron (todo)
 
 su www-data -s /bin/bash -c "touch /mnt/data/owncloud.log"
 tail -F /mnt/data/owncloud.log
